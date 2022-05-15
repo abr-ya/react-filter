364fa6683cda1330b456b1e1fbd0b8e85994c310
abr-ya <gamer05@list.ru>
2022-05-15 09:52:17 +0300

    feat: Create simple list and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,34 @@
-const App = (): JSX.Element => <div className="app">Hello, React!</div>;
+import { useState, useEffect } from "react";
+import { Users } from "./data/users";
+import { IUser } from "./interfaces";
+import "./app.css";
+
+function App(): JSX.Element {
+  const [query, setQuery] = useState("");
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    setUsers(
+      Users.filter((item) => item.first_name.toLowerCase().includes(query)),
+    );
+  }, [query]);
+
+  return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      <ul className="list">
+        {users.map((user: IUser) => (
+          <li className="listItem" key={user.id}>
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
